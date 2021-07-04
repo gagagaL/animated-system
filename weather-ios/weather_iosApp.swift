1e5000f7ff7869c9448b9f830a9e13b28853da1e
masaaki-kawaguchi <masaaki.kr8@gmail.com>
2021-07-04 22:22:54 +0900

    Initial Commit

---
+++ weather-ios/weather_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_iosApp.swift
+//  weather-ios
+//
+//  Created by Fuyu on 2021/07/04.
+//
+
+import SwiftUI
+
+@main
+struct weather_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
